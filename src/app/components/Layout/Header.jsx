6443ed03d6780ad0e871c1cae38c2d4166f20a8f
hafeed <hafeedummer@gmail.com>
2025-08-19 00:38:57 +0530

    ui change

--- src/app/components/Layout/Header.jsx
+++ src/app/components/Layout/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header = () => {
   return (
     <div className='shadow-[0px_8px_5px_-5px_rgba(0,0,0,0.2)] w-full'>
-      <span className="text-3xl text-white flex w-full p-4">PRODUCT</span>
+      <span className="text-3xl font-semibold flex w-full p-4 text-grey-900">PRODUCT</span>
     </div>
   )
 }
